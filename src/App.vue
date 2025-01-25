688557d4783913d2fcff3c7d193c1cbb6f128af4
William David Velasquez <wd@creativosdigitales.co>
2025-01-25 09:01:32 -0500

    first run

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SVGLayout msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SVGLayout from './components/SVGLayout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SVGLayout
   }
 }
 </script>
---
+++ src/components/SVGLayout.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="hello">
+    <svg width="200" height="200">
+      <circle cx="100" cy="100" r="50" stroke="black" stroke-width="3" fill="red" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SVGLayout',
+  props: {
+    location : String, // URL of the SVG table layout file
+    status : Object, // table status data obtained with the Table Status Service
+    table_statuses : Object, // A Dictionary with the styles for each of the possible table_status values
+    order_statuses : Object // A Dictionary with the styles to be applied on every order status value 
+  },
+
+/*  Events
+table-selected : Trigered when the user selects a table on the screen
+*/
+  methods : {
+/*    updateStatus( status_object ) {},   // Updates all tables with a full status JSON object for all tables
+    updateTable( table_id, table_status, order_status ) {}  // Updates the status of only one Table, given its ID */
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
